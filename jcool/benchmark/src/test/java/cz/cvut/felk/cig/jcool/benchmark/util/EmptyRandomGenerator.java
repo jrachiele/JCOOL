7605276a91c9484cec7ed42ed3e77f5458dc3340
Tomas Barton <barton.tomas@gmail.com>
2014-03-08 11:57:01 +0100

    removed obsolete exceptions

--- jcool/benchmark/src/test/java/cz/cvut/felk/cig/jcool/benchmark/util/EmptyRandomGenerator.java
+++ jcool/benchmark/src/test/java/cz/cvut/felk/cig/jcool/benchmark/util/EmptyRandomGenerator.java
@@ -1,97 +1,98 @@
 package cz.cvut.felk.cig.jcool.benchmark.util;
 
 import cz.cvut.felk.cig.jcool.core.RandomGenerator;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * Created by IntelliJ IDEA.
  * User: miklamar
  * Date: 23.2.2011
  * Time: 17:04
- * RandomGenerator providing empty implementation. Suitable for testing when ancestor overrides only necesary methods.
+ * RandomGenerator providing empty implementation. Suitable for testing when
+ * ancestor overrides only necessary methods.
  */
-public class EmptyRandomGenerator implements RandomGenerator{
+public class EmptyRandomGenerator implements RandomGenerator {
+
     public void setSeed(int seed) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public double nextRandom() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public boolean nextBoolean() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public boolean nextBoolean(double probability) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public byte nextByte() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public byte nextByte(byte maxExclusive) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public byte nextByte(byte minInclusive, byte maxExclusive) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public int nextInt() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public int nextInt(int maxExclusive) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public int nextInt(int minInclusive, int maxExclusive) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public long nextLong() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public long nextLong(long maxExclusive) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public long nextLong(long minInclusive, long maxExclusive) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public float nextFloat() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public float nextFloat(float maxExclusive) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public float nextFloat(float minInclusive, float maxExclusive) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public double nextDouble() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public double nextDouble(double maxExclusive) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public double nextDouble(double minInclusive, double maxExclusive) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public double nextGaussian() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public double nextGaussian(double mean, double standardDeviation) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
